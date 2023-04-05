89b37b7d27fedb6f851e700654cc69a922bdb3a3
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2023-04-05 14:44:43 -0400

    changes to home page, all links working

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,7 @@
 console.log(`my name is Leandro, I'm a software engineer based in the DMV area. I am open to new client. email me @ leandro.a3001@gmail.com`)
 // $('.carousel').carousel()
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'ar,bn,zh-CN,zh-TW,fi,ht,ko,ru,es,ur', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+    //https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes#Table_of_all_possible_two_letter_codes
+    //the script-> https://codepen.io/modernmatter/pen/XVqZpj
+}
