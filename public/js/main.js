5392678d62443c090af686c48d091a4e6b7f1d7d
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2023-04-07 11:50:34 -0400

    translate dropdown menu finished, all languages added

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,9 @@
 console.log(`my name is Leandro, I'm a software engineer based in the DMV area. I am open to new client. email me @ leandro.a3001@gmail.com`)
 // $('.carousel').carousel()
 function googleTranslateElementInit() {
-    new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'ar,bn,zh-CN,zh-TW,fi,ht,ko,ru,es,ur', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+    new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'af,ar,am,az,bn,be,bs,bg,ca,ceb,co,cs,cy,da,de,nl,ny,zh-CN,zh-TW,fa,fi,fil,fr,fy,ga,gd,gl,gu,ha,haw,hmh,hmi,hml,hmm,hmn,he,hi,ht,hr,hy,hu,id,ig,is,it,ja,jvn,ka,kn,kk,km,ko,ku,ky,lo,la,lv,lt,lb,mk,mg,ms,ml,mt,mi,mr,mn,my,ne,no,pa,pt,pl,ps,ro,ru,el,en,eo,et,es,eu,sr,sm,sn,sd,si,sq,sk,sl,so,sv,sw,su,tg,ta,tt,te,th,tr,uk,ur,uz,vi,xh,yi,yo,zh,zu,za', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+    //missing: Filipino, Hebrew, Javanese, Sesotho, 
     //https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes#Table_of_all_possible_two_letter_codes
+    //https://r12a.github.io/app-subtags/
     //the script-> https://codepen.io/modernmatter/pen/XVqZpj
 }
